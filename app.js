f6d62a18cab08af169c56c8d74a5365ee7c6b6cc
i3training <93472082+i3training@users.noreply.github.com>
2021-12-20 01:16:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use('/', route);
 
 // A route that says hello
 route.get('/', function(req, res) {
-    res.send('Hello! This is the index page for the app.\n');
+    res.send('Hello! This is the cobaaaaaaaaaaaaaaa cobaaa coba page for the app.\n');
 });
 
 // A route that returns readiness status
